65e2241991b530e3acdb30e45c767d887ee2ebe7
Laura Messa Travin <laura.travin@gmail.com>
2021-05-09 16:13:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,4 +24,7 @@ class Evergreen extends Tree{
     super( species )
     this.name = name
   }
+  static definition(){
+    return `${super.definition()} Evergreens keep their leaves all year round.`
+  }
 }
