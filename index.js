9efd8248ae631520a81603ab78d77e2eeb849cb4
Laura Messa Travin <laura.travin@gmail.com>
2021-05-09 16:12:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ class Tree{
   }
   static definition(){
     return 'A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.'
-
   }
 }
 
@@ -14,6 +13,9 @@ class Deciduous extends Tree{
   constructor( species, name ){
     super( species )
     this.name = name
+  }
+  static definition(){
+    return `${super.definition()} Deciduous trees shed their leaves annually.`
   }
 }
 
